0c940553d2dfc9761cba782c49172da0247620c2
milos <milos.991@gmail.com>
2022-04-12 17:27:56 +0200

    Project setup

---
+++ src/views/HomeView.vue
@@ -0,0 +1,15 @@
+<template>
+  <HelloWorld />
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default defineComponent({
+  name: 'HomeView',
+  components: {
+    HelloWorld
+  }
+})
+</script>
